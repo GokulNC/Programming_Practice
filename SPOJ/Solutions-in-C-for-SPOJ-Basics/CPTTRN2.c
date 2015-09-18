a2dda6c5d66f3e886af0bfb3f619c32c6f86c9ac
Gokul NC <gokulnc@ymail.com>
2015-09-18 15:39:51 +0530

    Create CPTTRN2.c

---
+++ SPOJ/Solutions-in-C-for-SPOJ-Basics/CPTTRN2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+    int n,i,j,k,m,l,c;
+    scanf("%d",&n);
+    for(m=0;m<n;m++) {
+        scanf("%d%d",&l,&c);
+        for(i=0;i<l;i++) {
+            for(j=0;j<c;j++) {
+                if(i==0||i==l-1) printf("*");
+                else if(j==0||j==c-1) printf("*");
+                else printf(".");
+            }
+            printf("\n");
+        }
+        printf("\n");
+    }
+}
