5dc2de026ca45aaa73243c9d888b854f8a36c057
Gokul NC <gokulnc@ymail.com>
2015-09-29 22:27:36 +0530

    http://www.spoj.com/problems/SMPCIRC/

--- SPOJ/Solutions-in-C-for-SPOJ-Basics/SMPCIRC.c
+++ SPOJ/Solutions-in-C-for-SPOJ-Basics/SMPCIRC.c
@@ -8,8 +8,8 @@ int main(void) {
         scanf("%d%d%d%d%d%d",&x1,&y1,&r1,&x2,&y2,&r2);
         dist=sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
         r=r1>r2?r1-r2:r2-r1;
-        if(dist<=r) printf("I\n");
-        else if(dist>r1+r2) printf("O\n");
-        else printf("E");
+        if(dist<r) printf("I\n");//Inside
+        else if(dist==r) printf("E\n");//Tangential
+        else printf("O");//Other Case like Overlap or Outside
     }
 }
